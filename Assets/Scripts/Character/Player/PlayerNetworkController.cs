d73c96e80d1669e942bfa283080e94e3eb5b2ec6
ChrisLubin <chrisx700@gmail.com>
2023-11-26 17:33:10 -0500

    Fixed player position desync when changing seats

---
+++ Assets/Scripts/Character/Player/PlayerNetworkController.cs
@@ -0,0 +1,41 @@
+using Unity.Netcode.Components;
+
+public class PlayerNetworkController : NetworkBehaviorAutoDisable<PlayerNetworkController>
+{
+    private NetworkTransform _networkTransform;
+    private PlayerInteractorController _interactorController;
+
+    private void Awake()
+    {
+        this._networkTransform = GetComponent<NetworkTransform>();
+        this._interactorController = GetComponent<PlayerInteractorController>();
+    }
+
+    protected override void OnOwnerNetworkSpawn()
+    {
+        this._interactorController.OnDidInteraction += this.OnDidInteraction;
+    }
+
+    public override void OnDestroy()
+    {
+        base.OnDestroy();
+
+        if (this.IsOwner)
+            this._interactorController.OnDidInteraction -= this.OnDidInteraction;
+    }
+
+    private void OnDidInteraction(InteractionType interaction)
+    {
+        switch (interaction)
+        {
+            case InteractionType.EnterVehicle:
+                this._networkTransform.Interpolate = false;
+                break;
+            case InteractionType.ExitVehicle:
+                this._networkTransform.Interpolate = true;
+                break;
+            default:
+                break;
+        }
+    }
+}
