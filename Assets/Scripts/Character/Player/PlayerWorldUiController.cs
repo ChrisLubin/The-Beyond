3fcbf05c2bea2c5e778cc2eb29f14333b68d8c50
ChrisLubin <chrisx700@gmail.com>
2024-01-05 21:57:43 -0500

    Fixed other players name not showing sometimes

--- Assets/Scripts/Character/Player/PlayerWorldUiController.cs
+++ Assets/Scripts/Character/Player/PlayerWorldUiController.cs
@@ -1,3 +1,4 @@
+using Cysharp.Threading.Tasks;
 using TMPro;
 using Unity.Netcode;
 using UnityEngine;
@@ -6,13 +7,16 @@ public class PlayerWorldUiController : NetworkBehaviour
 {
     [SerializeField] private TextMeshPro _name;
 
-    public override void OnNetworkSpawn()
+    public override async void OnNetworkSpawn()
     {
         base.OnNetworkSpawn();
 
         if (this.IsOwner)
             this._name.gameObject.SetActive(false);
         else
+        {
+            await UniTask.Delay(5000);
             this._name.text = MultiplayerSystem.Instance.GetPlayerUsername(this.OwnerClientId);
+        }
     }
 }
